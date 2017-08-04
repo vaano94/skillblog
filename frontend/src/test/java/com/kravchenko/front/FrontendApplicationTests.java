54c682e3d1f8bdea2b7e1f4e30e1111967e35868
vaano94 <vaano94@gmail.com>
2017-08-05 00:09:25 +0300

    Forgot a couple of files

---
+++ frontend/src/test/java/com/kravchenko/front/FrontendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kravchenko.front;
+
+import com.kravchenko.FrontendApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {FrontendApplication.class})
+public class FrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
